fb87387dbaed8a3c240a21bd74438aa13c4c44ef
RenanUcelliMorozini <renanmorozini@hotmail.com>
2021-03-23 01:59:33 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/renanucelli/aula/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.renanucelli.aula.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.renanucelli.aula.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+	
+}
