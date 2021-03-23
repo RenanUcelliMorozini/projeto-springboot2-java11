3453d3c1fbd7c68203d893c619ec6ec0286c0266
RenanUcelliMorozini <renanmorozini@hotmail.com>
2021-03-23 15:31:54 -0300

    OrderStatus enum

--- src/main/java/br/com/renanucelli/aula/course/entities/Order.java
+++ src/main/java/br/com/renanucelli/aula/course/entities/Order.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import br.com.renanucelli.aula.course.entities.enums.OrderStatus;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -24,6 +26,8 @@ public class Order implements Serializable {
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant instant;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -33,10 +37,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant instant, User client) {
+	public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.instant = instant;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +60,16 @@ public class Order implements Serializable {
 	public void setInstant(Instant instant) {
 		this.instant = instant;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+	}
+		}
 
 	public User getClient() {
 		return client;
@@ -88,5 +103,6 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
+
 
 }
